7ea50bccaba976ad96d6ad35ec2cc10880c5ec02
jason-gao <gaolu@yundun.com>
2020-12-22 00:06:44 +0800

    support:memcached:redis:driver

--- src/DomainWhiteSdk.php
+++ src/DomainWhiteSdk.php
@@ -9,122 +9,142 @@
 namespace DomainWhiteSdk;
 
 
-use DomainWhiteSdk\Cache\MemCacheStore;
 use DomainWhiteSdk\Http\RawRequest;
 use DomainWhiteSdk\HttpClients\GuzzleHttpClient;
 use DomainWhiteSdk\Logger\MonologLogger;
-
-class DomainWhiteSdk
-{
-    private static $config;
-    private static $cache;
-    private static $client;
-    public static  $token;
-
-    public function __construct()
-    {
-    }
-
-    public static function setConfig($config)
-    {
-        self::$config = $config;
-    }
-
-    public static function getNormalConfig()
-    {
-        return self::$config['normal'];
-    }
-
-    public static function getCache()
-    {
-        if (!self::$cache) {
-            MemCacheStore::setConf(self::$config);
-            self::$cache = MemCacheStore::getInstance(self::getNormalConfig()['memcache_key']);
-        }
-
-        return self::$cache;
-    }
-
-    public static function getLogger()
-    {
-        $logger = null;
-        if (self::getNormalConfig()['sdk_log_switch']) {
-            $logger = MonologLogger::getLoggerInstance(__CLASS__, self::getNormalConfig()['sdk_log']);
-        }
-
-        return $logger;
-    }
-
-    public static function getClient()
-    {
-        if (!self::$client) {
-            self::$client = new GuzzleHttpClient(null, self::getLogger());
-        }
-
-        return self::$client;
-    }
-
-    public static function token()
-    {
-        $url      = self::getNormalConfig()['token_url'];
-        $dateline = time();
-        $res      = self::getCache()->get('white_token');
-        if (empty($res) || (isset($res['expiry']) && $res['expiry'] < ($dateline - self::getNormalConfig()['expiry'] - 100))) {
-            $body = self::apiCall($url, 'post', [
-                'uname' => self::getNormalConfig()['uname'],
-                'upass' => self::getNormalConfig()['upass']
-            ], [
-                'Content-Type' => 'application/x-www-form-urlencoded'
-            ]);
-            $body = json_decode($body, true);
-            if (isset($body['status']) && $body['status'] == 1) {
-                $res = $body['data'];
-                self::getCache()->set('white_token', $res);
-            }
-        }
-        static::$token = isset($res['token']) ? $res['token'] : '';
-        return static::$token;
-    }
+use DomainWhiteSdk\Cache\MemCachedStore;
+use DomainWhiteSdk\Cache\RedisCacheStore;
+use DomainWhiteSdk\Cache\CacheStoreInterface;
 
 
-    public static function getRequestHeaders()
-    {
-        $header = array(
-            'Authorization' => static::token(),
-            'Content-Type'  => 'application/x-www-form-urlencoded'
-        );
+class DomainWhiteSdk {
+	private static $config;
+	private static $cache;
+	private static $client;
+	public static $token;
 
-        return $header;
-    }
+	public function __construct() {
+	}
 
-    public static function apiCall($url, $method, $body, $headers = [], $timeOut = 20, $options = [])
-    {
-        $url  = self::getNormalConfig()['base_api_url'] . $url;
-        $body = RawRequest::build_query($body);
-        if (strtolower($method) == 'get') {
-            $url .= $body;
-        }
-        $rawResponse = self::getClient()->send($url, $method, $body, $headers, $timeOut, $options);
-        $resBody     = $rawResponse->getBody();
+	public static function setConfig( $config ) {
+		self::$config = $config;
+	}
 
-        return $resBody;
-    }
-
-    public static function addDomainWhite($url = 'whitelist/add', $data = [])
-    {
-        $headers = self::getRequestHeaders();
-        $res     = self::apiCall($url, 'post', $data, $headers);
-
-        return $res;
-    }
+	public static function getNormalConfig() {
+		return self::$config['normal'];
+	}
 
 
-    public static function delDomainWhite($url = 'whitelist/del', $data = [])
-    {
-        $headers = self::getRequestHeaders();
-        $res     = self::apiCall($url, 'post', $data, $headers);
+	public static function setCache( CacheStoreInterface $cache ) {
+		self::$cache = $cache;
+	}
 
-        return $res;
-    }
+	public static function getCache() {
+		if ( ! self::$cache ) {
+			$driver = isset( self::getNormalConfig()['cache_driver'] ) ? self::getNormalConfig()['cache_driver'] : 'memcached';
+			if ( $driver == 'memcached' ) {
+				MemCachedStore::setConf( self::$config );
+				self::$cache = MemCachedStore::getInstance( 'memcache_domain_white' );
+			} else if ( $driver == 'redis' ) {
+				RedisCacheStore::setConf( self::$config );
+				self::$cache = RedisCacheStore::getInstance( 'redis_domain_white' );
+			}
+		}
+
+		return self::$cache;
+	}
+
+
+	public static function getLogger() {
+		$logger = null;
+		if ( self::getNormalConfig()['sdk_log_switch'] ) {
+			$logger = MonologLogger::getLoggerInstance( __CLASS__, self::getNormalConfig()['sdk_log'] );
+		}
+
+		return $logger;
+	}
+
+	public static function getClient() {
+		if ( ! self::$client ) {
+			self::$client = new GuzzleHttpClient( null, self::getLogger() );
+		}
+
+		return self::$client;
+	}
+
+	public static function token() {
+		$url            = self::getNormalConfig()['token_url'];
+		$dateline       = time();
+		$cacheRes       = self::getCache()->get( 'white_token' );
+		$cacheResDecode = self::jsonDecodeHold( $cacheRes );
+		if ( empty( $cacheResDecode ) || ( isset( $cacheResDecode['expiry'] ) && $cacheResDecode['expiry'] < ( $dateline - self::getNormalConfig()['token_expiry'] - 100 ) ) ) {
+			$body       = self::apiCall( $url, 'post', [
+				'uname' => self::getNormalConfig()['uname'],
+				'upass' => self::getNormalConfig()['upass']
+			], [
+				'Content-Type' => 'application/x-www-form-urlencoded'
+			] );
+			$bodyDecode = self::jsonDecodeHold( $body );
+			if ( isset( $bodyDecode['status'] ) && $bodyDecode['status'] == 1 ) {
+				$cacheResDecode = $bodyDecode['data'];
+				$cacheRes       = self::jsonEncodeHold( $cacheResDecode );
+				self::getCache()->set( 'white_token', $cacheRes );
+			}
+		}
+		static::$token = isset( $cacheResDecode['token'] ) ? $cacheResDecode['token'] : '';
+
+		return static::$token;
+	}
+
+
+	public static function getRequestHeaders() {
+		$header = array(
+			'Authorization' => static::token(),
+			'Content-Type'  => 'application/x-www-form-urlencoded'
+		);
+
+		return $header;
+	}
+
+	public static function apiCall( $url, $method, $body, $headers = [], $timeOut = 20, $options = [] ) {
+		$url  = self::getNormalConfig()['base_api_url'] . $url;
+		$body = RawRequest::build_query( $body );
+		if ( strtolower( $method ) == 'get' ) {
+			$url .= $body;
+		}
+		$rawResponse = self::getClient()->send( $url, $method, $body, $headers, $timeOut, $options );
+		$resBody     = $rawResponse->getBody();
+
+		return $resBody;
+	}
+
+	public static function addDomainWhite( $url = 'whitelist/add', $data = [] ) {
+		$headers = self::getRequestHeaders();
+		$res     = self::apiCall( $url, 'post', $data, $headers );
+
+		return $res;
+	}
+
+
+	public static function delDomainWhite( $url = 'whitelist/del', $data = [] ) {
+		$headers = self::getRequestHeaders();
+		$res     = self::apiCall( $url, 'post', $data, $headers );
+
+		return $res;
+	}
+
+	public static function jsonEncodeHold( $data, $isPretty = 0 ) {
+		if ( $isPretty ) {
+			return json_encode( $data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT );
+		} else {
+			return json_encode( $data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES );
+		}
+	}
+
+	public static function jsonDecodeHold( $data, $assoc = true ) {
+		$res = json_decode( $data, $assoc );
+
+		return $res;
+	}
 
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,8 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
+error_reporting(E_ERROR);
+date_default_timezone_set('Asia/Shanghai');
 
 abstract class TestCase extends BaseTestCase
 {
